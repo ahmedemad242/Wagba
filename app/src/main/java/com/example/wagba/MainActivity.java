50f5a835e1bf6f78e49aef1b0374beb79afd782e
Ahmed Emad <ahmedemad242@gmail.com>
2022-12-08 19:52:55 +0200

    feat: add food recycler view

--- app/src/main/java/com/example/wagba/MainActivity.java
+++ app/src/main/java/com/example/wagba/MainActivity.java
@@ -1,16 +1,44 @@
 package com.example.wagba;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import android.os.Bundle;
+
+import com.example.wagba.adapter.FoodAdapter;
+import com.example.wagba.databinding.ActivityMainBinding;
+import com.example.wagba.model.Food;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    private ActivityMainBinding activityMainBinding;
+    private FoodAdapter foodAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+        activityMainBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(activityMainBinding.getRoot());
+
+        List<Food> foodList = new ArrayList<Food>();
+        foodList.add(new Food("Cake", "7.43 LE", R.drawable.ic_launcher_background));
+        foodList.add(new Food("Food", "24.72 LE", R.drawable.ic_launcher_background));
+        foodList.add(new Food("Italian pasta", "119.1 LE", R.drawable.ic_launcher_background));
+        foodList.add(new Food("LOL", "1219.1 LE", R.drawable.ic_launcher_background));
+
+        setFoodRecycler(foodList);
+
+    }
+
+    private void setFoodRecycler(List<Food>foodList){
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this, RecyclerView.HORIZONTAL, false);
+        activityMainBinding.foodRecycleView.setLayoutManager(layoutManager);
+        foodAdapter = new FoodAdapter(this, foodList);
+        activityMainBinding.foodRecycleView.setAdapter(foodAdapter);
     }
 }
---
+++ app/src/main/java/com/example/wagba/adapter/FoodAdapter.java
@@ -0,0 +1,61 @@
+package com.example.wagba.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.wagba.R;
+import com.example.wagba.model.Food;
+
+import java.util.List;
+
+public class FoodAdapter extends RecyclerView.Adapter<FoodAdapter.FoodViewHolder> {
+    private Context context;
+    private List<Food> foodList;
+
+    public FoodAdapter(Context context, List<Food> foodList) {
+        this.context = context;
+        this.foodList = foodList;
+    }
+
+    @NonNull
+    @Override
+    public FoodViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.food_row_item, parent,false);
+        return new FoodViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull FoodViewHolder holder, int position) {
+        holder.image.setImageResource(foodList.get(position).getImageUrl());
+        holder.name.setText(foodList.get(position).getName());
+        holder.price.setText(foodList.get(position).getPrice());
+    }
+
+    @Override
+    public int getItemCount() {
+        return foodList.size();
+    }
+
+    public static final class FoodViewHolder extends RecyclerView.ViewHolder{
+
+        ImageView image;
+        TextView price, name;
+
+        public FoodViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            image = itemView.findViewById(R.id.image);
+            name = itemView.findViewById(R.id.name);
+            price = itemView.findViewById(R.id.price);
+
+        }
+    }
+
+}
