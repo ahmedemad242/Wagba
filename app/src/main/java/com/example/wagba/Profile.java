dacf01cd75f70cd3d67c148ed2d390c304a5c6cd
Ahmed Emad <ahmedemad242@gmail.com>
2022-12-15 13:14:09 +0200

    feat: add user profile from firebase

--- app/src/main/java/com/example/wagba/Profile.java
+++ app/src/main/java/com/example/wagba/Profile.java
@@ -2,20 +2,29 @@ package com.example.wagba;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.Window;
 
 import com.example.wagba.databinding.ActivityProfileBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Profile extends AppCompatActivity {
 
     private ActivityProfileBinding activityProfileBinding;
     private Window window;
+    FirebaseUser user;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        String name = user.getDisplayName();
+        String email = user.getEmail();
+
         activityProfileBinding = ActivityProfileBinding.inflate(getLayoutInflater());
         getSupportActionBar().setTitle("");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
@@ -24,6 +33,10 @@ public class Profile extends AppCompatActivity {
 
         WindowController.changeNavigationBarColor(window, getResources().getColor(R.color.white));
         WindowController.changeStatusBarColor(window, getResources().getColor(R.color.dark_blue), false);
+
+        activityProfileBinding.profileName.setText(name.split(" ")[0]+"!");
+        activityProfileBinding.profileFullName.setText(name);
+        activityProfileBinding.profileEmail.setText(email);
     }
 
     @Override
