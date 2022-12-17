09ca0f80081da4b9d3a49085ce0315b96bc359e2
Ahmed Emad <ahmedemad242@gmail.com>
2022-12-18 00:16:02 +0200

    feat: independent load of food from restaurants

--- app/src/main/java/com/example/wagba/RestaurantDetails.java
+++ app/src/main/java/com/example/wagba/RestaurantDetails.java
@@ -15,7 +15,13 @@ import com.example.wagba.model.Food;
 import com.example.wagba.model.Restaurant;
 import com.example.wagba.utils.ImageUtils;
 import com.example.wagba.utils.WindowController;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class RestaurantDetails extends AppCompatActivity {
@@ -23,6 +29,7 @@ public class RestaurantDetails extends AppCompatActivity {
     private ActivityResturantDetailsBinding activityResturantDetailsBinding;
     private RestaurantFoodAdapter restaurantFoodAdapter;
     private Window window;
+    DatabaseReference foodRef;
 
 
     @Override
@@ -37,24 +44,45 @@ public class RestaurantDetails extends AppCompatActivity {
 
         Restaurant restaurant = getIntent().getParcelableExtra("restaurant");
 
+        String path = "/restaurants/" + restaurant.getId() + "/menuItems";
+        foodRef = FirebaseDatabase.getInstance("https://wagba-c33d4-default-rtdb.firebaseio.com/").getReference(path);
+
+        List<Food> foodList = new ArrayList<>();
+        foodRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+                // Retrieve the list of dishes from the snapshot
+
+                for (DataSnapshot dishSnapshot : snapshot.getChildren()) {
+                    Food food = dishSnapshot.getValue(Food.class);
+                    foodList.add(food);
+                }
+                setFoodRecycler(restaurant, foodList);
+            }
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Handle errors here
+            }});
+
         ImageUtils.loadImage(this, restaurant.getImageUrl(),
                 activityResturantDetailsBinding.restaurantDetailsImage, R.drawable.logo_bg_light);
 
         activityResturantDetailsBinding.restaurantDetailsDish.setText(
-                String.valueOf(restaurant.getMenuItems().size()));
+                String.valueOf(foodList.size()));
         activityResturantDetailsBinding.restaurantDetailsRating.setText(restaurant.getRating());
+        //TODO:: Fill order number
         activityResturantDetailsBinding.restaurantDetailsOrders.setText("10");
-        setFoodRecycler(restaurant);
+
 
         activityResturantDetailsBinding.restaurantFoodHome.setOnClickListener(v -> {
             onBackPressed();
         });
     }
 
-    private void setFoodRecycler(Restaurant restaurant){
+    private void setFoodRecycler(Restaurant restaurant, List<Food> foodList){
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this, RecyclerView.VERTICAL, false);
         activityResturantDetailsBinding.restaurantFoodRecyclerView.setLayoutManager(layoutManager);
-        restaurantFoodAdapter = new RestaurantFoodAdapter(this, restaurant);
+        restaurantFoodAdapter = new RestaurantFoodAdapter(this, restaurant, foodList);
         activityResturantDetailsBinding.restaurantFoodRecyclerView.setAdapter(restaurantFoodAdapter);
     }
 
